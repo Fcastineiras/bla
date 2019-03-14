07e8ffb87fdcdeb986ec71b378d7ac925140a02d
Fiamma Castiñeiras <fiamma.castineiras@wenance.com>
2019-03-14 12:50:49 -0300

    s

--- src/main/java/com/wenance/webhook/consumer/controller/WebhookConsumer.java
+++ src/main/java/com/wenance/webhook/consumer/controller/WebhookConsumer.java
@@ -4,6 +4,7 @@ import com.wenance.webhook.consumer.model.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,6 +16,14 @@ public class WebhookConsumer {
     @PostMapping(path = "/webhook")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Response> health() {
+        Response response = new Response("test auto deploy");
+        return ResponseEntity.ok(response);
+    }
+
+
+    @GetMapping(path = "/webhook")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Response> responseGet() {
         Response response = new Response("test auto deploy");
         return ResponseEntity.ok(response);
     }
