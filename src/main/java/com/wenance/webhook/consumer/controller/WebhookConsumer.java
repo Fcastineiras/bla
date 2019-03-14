5873f3b6377470cdc2dba615e5fadd42f9db1073
Fiamma Castiñeiras <fiamma.castineiras@wenance.com>
2019-03-14 12:35:45 -0300

    add post controller

--- src/main/java/com/wenance/webhook/consumer/controller/WebhookConsumer.java
+++ src/main/java/com/wenance/webhook/consumer/controller/WebhookConsumer.java
@@ -4,7 +4,7 @@ import com.wenance.webhook.consumer.model.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RequestMapping(value = "/api/v1", produces = "application/json")
 public class WebhookConsumer {
 
-    @GetMapping(path = "/webhook")
+    @PostMapping(path = "/webhook")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Response> health() {
         Response response = new Response("test auto deploy");
